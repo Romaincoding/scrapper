2f702667143be74b9bba006ebd1308bb1cd7c902
Romain <romain.dupuis@edu.itescia.fr>
2020-11-17 09:38:39 +0100

    refacto

--- src/Main.java
+++ src/Main.java
@@ -12,42 +12,38 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 
 
-        String fdj = "https://www.fdj.fr/jeux-de-tirage/loto/resultats";
+        final String fdj = "https://www.fdj.fr/jeux-de-tirage/loto/resultats";
 
         // Open a new browser window
         FirefoxDriver driver = new FirefoxDriver();
         WebDriverWait wait = new WebDriverWait(driver, 30);
 
-        driver.navigate().to("https://www.fdj.fr/jeux-de-tirage/loto/resultats");
+        driver.navigate().to(fdj);
 
         //------------------- française des jeux-----------------------
         Thread.sleep(5000);
-        final String text = driver.findElement(By.cssSelector("div.result-content:nth-child(2) > div:nth-child(1) > div:nth-child(1) > span:nth-child(1)")).getText();
-        final String text1 = driver.findElement(By.cssSelector("div.result-content:nth-child(2) > div:nth-child(1) > div:nth-child(2) > span:nth-child(1)")).getText();
-        final String text2 = driver.findElement(By.cssSelector("div.result-content:nth-child(2) > div:nth-child(1) > div:nth-child(3) > span:nth-child(1)")).getText();
-        final String text3 = driver.findElement(By.cssSelector("div.result-content:nth-child(2) > div:nth-child(1) > div:nth-child(4) > span:nth-child(1)")).getText();
-        final String text4 = driver.findElement(By.cssSelector("div.result-content:nth-child(2) > div:nth-child(1) > div:nth-child(5) > span:nth-child(1)")).getText();
-        final String text5 = driver.findElement(By.cssSelector("span.numbers-item_num:nth-child(2)")).getText();
+        final String firstNumber = driver.findElement(By.cssSelector("div.result-content:nth-child(2) > div:nth-child(1) > div:nth-child(1) > span:nth-child(1)")).getText();
+        final String secondNumber = driver.findElement(By.cssSelector("div.result-content:nth-child(2) > div:nth-child(1) > div:nth-child(2) > span:nth-child(1)")).getText();
+        final String thirdNumber = driver.findElement(By.cssSelector("div.result-content:nth-child(2) > div:nth-child(1) > div:nth-child(3) > span:nth-child(1)")).getText();
+        final String fourthNumber = driver.findElement(By.cssSelector("div.result-content:nth-child(2) > div:nth-child(1) > div:nth-child(4) > span:nth-child(1)")).getText();
+        final String fifthNumber = driver.findElement(By.cssSelector("div.result-content:nth-child(2) > div:nth-child(1) > div:nth-child(5) > span:nth-child(1)")).getText();
+        final String sixthNumber = driver.findElement(By.cssSelector("span.numbers-item_num:nth-child(2)")).getText();
         WebElement field = driver.findElement(By.cssSelector(".header-search-desktop > div:nth-child(1) > div:nth-child(1) > input:nth-child(1)"));
         Actions builder = new Actions(driver);
 
-        String val = "la grotte aux dragons";
-        for (int i = 0; i < val.length(); i++){
-            char c = val.charAt(i);
+        final String val = "la grotte aux dragons";
+        for (char c : val.toCharArray()) {
             String s = new StringBuilder().append(c).toString();
             field.sendKeys(s);
             Thread.sleep(1000);
+
         }
 
-        //--------------------------Tv time ------------------------
 
-
-
-
-        Actions seriesOfActions = builder.moveToElement(field).click().sendKeys(field,"la grotte aux dragons");
-        Thread.sleep(1000);
+        Actions seriesOfActions = builder.moveToElement(field).click().sendKeys(field, "la grotte aux dragons");
+        Thread.sleep(200);
         seriesOfActions.perform();
-       String result = text + " " + text1 + " " + text2 +" " + text3 + " " + text4 + " " + text5;
+        final String result = firstNumber + " " + secondNumber + " " + thirdNumber + " " + fourthNumber + " " + fifthNumber + " " + sixthNumber;
         System.out.println(result);
 
         // Close the browser window
